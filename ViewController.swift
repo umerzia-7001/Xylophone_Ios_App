8ff44ffcf997c97ce9412cd1e77159ba6f0e98b3
muhammad Umer Zia <55735751+umerzia-7001@users.noreply.github.com>
2020-08-30 20:58:52 +0500

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print(sender.currentTitle as Any)
+        playSound(soundName:sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
+
